9ce29610564807f3eefcb1398550453bdd975940
m1_air <m1_air@m1-airs-Air.hsd1.co.comcast.net>
2024-03-11 15:41:59 -0600

    initial project files

---
+++ upgraded-couscous/upgraded-couscous/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  upgraded-couscous
+//
+//  Created by m1_air on 3/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ upgraded-couscous/upgraded-couscous/upgraded_couscousApp.swift
@@ -0,0 +1,17 @@
+//
+//  upgraded_couscousApp.swift
+//  upgraded-couscous
+//
+//  Created by m1_air on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct upgraded_couscousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
